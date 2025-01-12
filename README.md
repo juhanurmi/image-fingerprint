546538e13b8a81fbf1f5d628c1c87f0e8ae9bda0
juhanurmi <juha.nurmi@ahmia.fi>
2025-01-12 11:03:59 +0200

    Metadata fetcher and image sampling for comparison.

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 This script retrieves all images from a given URL and downloads samples of the file from the beginning and the end.
 The EXIF metadata is located at the beginning of the image file.
 The system generates fingerprints by utilizing the image data.
+Ignore small images.
 The system generates a JSON file to store both the fingerprint and its metadata.
 
 ## Install
@@ -26,42 +27,67 @@ ls ./data/
 ```json
 [
     {
-        "url": "https://github.com/ianare/exif-samples/blob/master/jpg/Ricoh_Caplio_RR330.jpg?raw=true",
-        "image_size": 3662,
+        "url": "https://exiv2.org/include/img_1771.jpg",
+        "image_size": 32764,
         "exif": {
             "ResolutionUnit": "2",
-            "ExifOffset": "190",
-            "Make": "Caplio ",
-            "Model": "RR330      ",
-            "Software": "GIMP 2.4.5",
-            "DateTime": "2008:07:31 17:36:21",
-            "YCbCrPositioning": "2",
-            "XResolution": "72.0",
-            "YResolution": "72.0",
+            "ExifOffset": "196",
+            "Make": "Canon",
+            "Model": "Canon PowerShot S40",
+            "Orientation": "1",
+            "DateTime": "2003:12:14 12:01:44",
+            "YCbCrPositioning": "1",
+            "XResolution": "180.0",
+            "YResolution": "180.0",
             "ExifVersion": "0220",
             "ComponentsConfiguration": "\u0001\u0002\u0003\u0000",
-            "ShutterSpeedValue": "5.1",
-            "DateTimeOriginal": "2004:08:31 19:52:58",
-            "DateTimeDigitized": "2004:08:31 19:52:58",
-            "ApertureValue": "4.0",
-            "FlashPixVersion": "0100",
+            "CompressedBitsPerPixel": "5.0",
+            "DateTimeOriginal": "2003:12:14 12:01:44",
+            "DateTimeDigitized": "2003:12:14 12:01:44",
+            "ShutterSpeedValue": "8.96875",
+            "ApertureValue": "4.65625",
+            "ExposureBiasValue": "0.0",
+            "MaxApertureValue": "2.970855712890625",
+            "MeteringMode": "2",
+            "Flash": "24",
+            "FocalLength": "21.3125",
+            "UserComment": "...",
             "ColorSpace": "1",
-            "ExifImageWidth": "100",
-            "Flash": "16",
-            "ExifImageHeight": "75",
-            "ExifInteroperabilityOffset": "554",
+            "ExifImageWidth": "2272",
+            "FocalPlaneXResolution": "8114.285714285715",
+            "ExifImageHeight": "1704",
+            "FocalPlaneYResolution": "8114.285714285715",
+            "FocalPlaneResolutionUnit": "2",
+            "SensingMethod": "2",
+            "FileSource": "\u0003",
+            "ExposureTime": "0.002",
+            "ExifInteroperabilityOffset": "1416",
+            "FNumber": "4.9",
+            "CustomRendered": "0",
+            "ExposureMode": "0",
+            "FlashPixVersion": "0100",
             "WhiteBalance": "0",
-            "SceneCaptureType": "0",
-            "Sharpness": "0",
-            "SubjectDistanceRange": "3",
-            "ExposureTime": "0.030303030303030304",
-            "FNumber": "2.88",
-            "ImageUniqueID": "00000000000000000000000000000111",
-            "ISOSpeedRatings": "100",
-            "ExposureMode": "0"
+            "DigitalZoomRatio": "1.0",
+            "MakerNote": "...",
+            "SceneCaptureType": "0"
         },
-        "timestamp": "2025-01-11T09:20:42",
-        "sha256_test_5000_bytes": "e920d750c491f3088eeb0f31fb4659164755af11e4bbbe269430f32c3ae10928"
+        "timestamp": "2025-01-12T10:54:29",
+        "sha256_first_10240_bytes": "c629c7247153f59599727db141ee1f3c5e4a5a3cc90fa88e2e97d3d78c2fc823",
+        "sha256_last_1024_bytes": "52efda24efb2b1e1534f898b382b9ecbc0747f96d7da554602bdf295a486e47b",
+        "random_128_bytes_sample_start": "ef4345790e09c77cf4a2e3cc14721b0143423972c493ee4d28cb372fa5712079472efdeb8f9463f31e7e95007123e51c87eb4f18501bed9a62ef9276039d389c8c9fa52602861c8f3a7863a800dcb9e2859017511bf4a407a8a2e80b4b7b8182adb8ebfde9ceaca79e41dc11d45572394218019a9f04c258b436c7a1ec6b6c73",
+         "random_128_bytes_sample_end": "cb44f02d651b15cd558b9320531417c8c9cd6b67e13138cae2ab1f819d648cd161664256f0ae0eae46a54572855551b2073a3f1fe1cd6ba1b1b1e75516e02d62f860c0b29dcf4142ce49c5757566fa3906eea7239f7a53a5d7070adfcdd0fbd7575480cc323608debb39fef5d5d4843082a46723b114a59643863a5bf9bbfbd7"
     }
 ]
+```
+
+# Test images
+
+Using two random 128-byte samples (one from the start and one from the end) of an image to determine whether the image is the same is a **reasonable heuristic**, but it is not a foolproof method.
+
+```sh
+wget "https://exiv2.org/include/img_1771.jpg"
+
+xxd -p img_1771.jpg | tr -d '\n' | grep -o "cb44f02d651b15cd558b9320531417c8c9cd6b67e13138cae2ab1f819d648cd161664256f0ae0eae46a54572855551b2073a3f1fe1cd6ba1b1b1e75516e02d62f860c0b29dcf4142ce49c5757566fa3906eea7239f7a53a5d7070adfcdd0fbd7575480cc323608debb39fef5d5d4843082a46723b114a59643863a5bf9bbfbd7"
+
+xxd -p img_1771.jpg | tr -d '\n' | grep -o "ef4345790e09c77cf4a2e3cc14721b0143423972c493ee4d28cb372fa5712079472efdeb8f9463f31e7e95007123e51c87eb4f18501bed9a62ef9276039d389c8c9fa52602861c8f3a7863a800dcb9e2859017511bf4a407a8a2e80b4b7b8182adb8ebfde9ceaca79e41dc11d45572394218019a9f04c258b436c7a1ec6b6c73"
 ```
--- metadata_fetcher.py
+++ metadata_fetcher.py
@@ -1,13 +1,13 @@
 """
-This script retrieves all images from a given URL and downloads:
-1. The EXIF metadata from the beginning of the image file.
-2. A 5KB sample of the binary content.
-The metadata are saved in JSON format.
+This script partially downloads samples of all images from a given URL.
+Extracts the EXIF metadata from the beginning of the image file and samples of the content.
+Save the metadata in JSON format.
+Ignore small images.
 """
 import io
 import os
 import json
-import hashlib
+from hashlib import sha256
 import datetime
 from urllib.parse import urljoin, urlparse
 import requests
@@ -15,75 +15,129 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 from bs4 import BeautifulSoup
 
-def download_image_metadata(url):
+def download_image_metadata(resource_url):
     """Fetch image metadata."""
+    results = []
+    if "image" in head(resource_url).get("content-type", ""): # Image link
+        results = fetch_images([resource_url], resource_url)
+    elif "html" in head(resource_url).get("content-type", ""): # HTML page
+        response = requests.get(resource_url, allow_redirects=True, timeout=60)
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch URL: {resource_url}")
+        soup = BeautifulSoup(response.content, "html.parser")
+        img_tags = soup.find_all("img")
+        results = fetch_images(img_tags, resource_url)
+    save_results(results, resource_url)
+
+def save_results(results, url):
+    """Save results to JSON."""
     if not os.path.isdir("./data/"):
         os.makedirs("./data/")
     domain_main = urlparse(url).netloc.split('.')[-2]
     if not os.path.isdir(f"./data/{domain_main}"):
         os.makedirs(f"./data/{domain_main}")
-    response = requests.get(url, timeout=180)
-    if response.status_code != 200:
-        raise Exception(f"Failed to fetch URL: {url}")
-    soup = BeautifulSoup(response.content, "html.parser")
-    img_tags = soup.find_all("img")
-    results = fetch_images(img_tags, url)
-    filename = hashlib.sha256(url.encode("utf-8")).hexdigest()[0:10]
+    filename = sha256(url.encode("utf-8")).hexdigest()[0:10]
     filepath = f"./data/{domain_main}/{filename}.json"
-    save_results(results, filepath)
-
-def save_results(results, filepath):
-    """Save results to JSON."""
     with open(filepath, "w", encoding="utf-8") as json_file:
         json.dump(results, json_file, indent=4)
     print(f"Metadata saved to {filepath}")
+
+def partial_download(img_url, start, end):
+    """Process images one by one."""
+    img_bytes = b""
+    try:
+        response = requests.get(img_url, stream=True, allow_redirects=True, timeout=60,
+                                headers={"Range": f"bytes={start}-{end}"})
+        if response.status_code in [200, 206]: # 206 indicates partial content
+            return response.content
+        print(f"HTTP response code: {response.status_code}")
+    except Exception as error:
+        print(f"Partial download failed: {str(error)}")
+        print(f"Bytes={start}-{end} URL: {img_url}")
+    return img_bytes
+
+def head(resource_url):
+    """Process images one by one."""
+    try:
+        response = requests.head(resource_url, allow_redirects=True, timeout=60)
+        if response.status_code in [200, 206]: # 206 indicates partial content
+            return response.headers
+        print(f"HTTP response code: {response.status_code}")
+        return {}
+    except Exception as error:
+        print(f"Head failed: {str(error)}")
+        print(f"URL: {resource_url}")
+    return {}
+
+def extract_metadata(start_bytes):
+    ''' Extract EXIF metadata '''
+    exif_data = {}
+    try:
+        img = Image.open(io.BytesIO(start_bytes))
+        exif_raw = img._getexif()
+        if exif_raw:
+            for tag, value in exif_raw.items():
+                # Convert EXIF values to JSON-serializable types
+                if isinstance(value, (bytes, bytearray)):
+                    value = value.decode(errors="ignore")  # Decode bytes if possible
+                else:
+                    value = str(value)  # Convert other types to string
+                exif_data[TAGS.get(tag, tag)] = value
+    except Exception as error:
+        print(f"Failed to extract EXIF: {str(error)}")
+    return exif_data
+
+def raw_image_data(start_bytes):
+    """ Extract the start of the image data """
+    image_data = b""
+    try:
+        img = Image.open(io.BytesIO(start_bytes))
+        image_content = io.BytesIO()
+        img.save(image_content, format=img.format)
+        image_data = image_content.getvalue()
+    except Exception as error:
+        print(f"Failed to extract raw image data: {str(error)}")
+    return image_data
 
 def fetch_images(img_tags, base_url):
     """Process images one by one."""
     results = []
     for img_tag in img_tags:
-        img_url = img_tag.get("src")
+        if img_tags[0] == base_url:
+            img_url = base_url
+        else:
+            img_url = img_tag.get("src")
         if img_url:
-            img_url = urljoin(base_url, img_url)  # Resolve full image URL
+            if img_url != base_url:
+                img_url = urljoin(base_url, img_url) # Resolve full image URL
             print(f"Processing image: {img_url}")
-            try:
-                # Fetch only the first 5KB
-                img_response = requests.get(img_url, stream=True, timeout=180,
-                                            headers={"Range": "bytes=0-5120"})
-                if img_response.status_code not in [200, 206]:  # 206 indicates partial content
-                    continue
-                img_bytes = img_response.content
+            total_size = int(head(img_url).get("Content-Length", "0"))
+            if total_size < 20480: # Ignore images smaller than 20KB
+                print(f"Ignore images smaller than 20480 bytes: img is {total_size} bytes")
+                continue
 
-                # Extract EXIF metadata
-                exif_data = {}
-                try:
-                    img = Image.open(io.BytesIO(img_bytes))
-                    exif_raw = img._getexif()
-                    if exif_raw:
-                        for tag, value in exif_raw.items():
-                            # Convert EXIF values to JSON-serializable types
-                            if isinstance(value, (bytes, bytearray)):
-                                value = value.decode(errors="ignore")  # Decode bytes if possible
-                            else:
-                                value = str(value)  # Convert other types to string
-                            exif_data[TAGS.get(tag, tag)] = value
-                except Exception as error:
-                    exif_data = f"Failed to extract EXIF: {str(error)}"
-
-                # Fetch full image size (if available)
-                full_size = int(img_response.headers.get("Content-Length", 0))
-
-                # Append result
-                results.append({
-                    "url": img_url,
-                    "image_size": full_size,
-                    "exif": exif_data,
-                    "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-                    "sha256_test_5000_bytes": hashlib.sha256(img_bytes).hexdigest(),
-                    #"binary_test_download": img_bytes.hex(),
-                })
-            except Exception as error:
-                print(f"Failed to process image {img_url}: {error}")
+            start_bytes = partial_download(img_url, 0, 10240) # First 10KB of the image
+            if not start_bytes:
+                print("Error: no start bytes to process.")
+                continue
+            exif_data = extract_metadata(start_bytes)
+            start_sample = start_bytes[-128:].hex() # 128 bytes sample
+            last_bytes = partial_download(img_url, total_size - 1024, total_size - 1) #Last 1KB
+            # Generate hashes
+            sha256_first_bytes = sha256(start_bytes).hexdigest()
+            sha256_last_bytes = sha256(last_bytes).hexdigest() if last_bytes else None
+            end_sample = last_bytes[384:512].hex() # 128 bytes sample
+            # Append result
+            results.append({
+                "url": img_url,
+                "image_size": total_size,
+                "exif": exif_data,
+                "timestamp": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                "sha256_first_10240_bytes": sha256_first_bytes,
+                "sha256_last_1024_bytes": sha256_last_bytes,
+                "random_128_bytes_sample_start": start_sample,
+                "random_128_bytes_sample_end": end_sample
+            })
     return results
 
 def read_urls_from_file(file_path):
